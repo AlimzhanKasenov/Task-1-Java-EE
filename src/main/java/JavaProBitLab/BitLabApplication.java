aada9599fc94172e4611b6c286ba2dbe6a167390
Алим <Rhbcnbyf414831>
2022-06-20 14:58:55 +0600

    Good

---
+++ src/main/java/JavaProBitLab/BitLabApplication.java
@@ -0,0 +1,13 @@
+package JavaProBitLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitLabApplication.class, args);
+	}
+
+}
